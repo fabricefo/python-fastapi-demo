661cdf9f4720dbd766e4ad42d55ae2dacffd5ad0
Fabrice Fourel <ffourel@gmail.com>
2023-10-30 13:04:06 +0100

    Refactor database

---
+++ database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+# Create a sqlite engine instance
+engine = create_engine("sqlite:///mytodo.db")
+
+# Create a DeclarativeMeta instance
+Base = declarative_base()
+
+# Define To Do class inheriting from Base
+class ToDo(Base):
+    __tablename__ = 'todos'
+    id = Column(Integer, primary_key=True)
+    task =  Column(String(50))
+
+# Create the database
+Base.metadata.create_all(engine)
